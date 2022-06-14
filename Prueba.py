8337343f691acc23f3a27e122faa3fd196068dd5
Joaquin Aguilera <joacoaguilera03@gmail.com>
2022-06-14 10:51:16 -0300

    Cambios de hoy

--- Prueba.py
+++ Prueba.py
@@ -2,20 +2,22 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-
 # Config de la pagina 
 st.set_page_config(
-     page_title="AirBnB (L.A)",
-     page_icon="🌎",
-     layout="wide",
-     initial_sidebar_state="auto",
+     page_title = "AirBnB (L.A)",
+     page_icon = "🌎",
+     layout = "wide",
+     initial_sidebar_state = "auto",
  ) 
 
-# Titulo
-st.title("Las Vegas")
+# Título
+st.title("Los Angeles")
 df = pd.read_csv('listings.csv')
 
-values = st.slider('Select a range of values', 0.0, 25000.0, (6250.0, 18750.0))
+# Slider segun el precio de cada tipo de hospedaje
+values = st.slider('Select a range of values', 0.0, 25000.0, (6250.0, 18750.0)) 
+# modificar parametros con una lista
 st.write('Values:', values)
 
+# Mapa
 st.map(df[(df["price"] < values[1]) & (df["price"] > values[0])])
--- Slider.py
+++ Slider.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import matplotlib as mp
-import streamlit as st 
-
 
 filename = 'listings.csv'
 df = pd.read_csv(filename, header=0)
