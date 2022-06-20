1243cd02564fe209227fa0d2ef98abd73dfa2064
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2022-06-20 16:09:18 -0300

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -13,7 +13,7 @@ st.set_page_config(
 st.title("Los Angeles")
 df = pd.read_csv('listings.csv')
 
-# Slider segun la cantidad de reviews de cada tipo de hospedaje
+#Constante para determinar el max y min de los precios
 valor_min = float(df["price"].min())
 valor_max = float(df["price"].max())
 
@@ -21,25 +21,30 @@ valor_max = float(df["price"].max())
 
 
 # Barrio
-option = st.selectbox(
+barrio = st.selectbox(
      'Selecciona un barrio:',
      sorted(list(set(list(df['neighbourhood'])))))
 
 # Tipo de hospedaje
-option1 = st.multiselect(
+hospedaje = st.multiselect(
      'Selecciona tipo de hospedaje',
      sorted(list(set(list(df['room_type'])))),
      ["Entire home/apt"])
 
+#Slider para determinar el maximo y minimo de precios
 values = st.slider('Elige un rango de precio', valor_min, valor_max, ((valor_max / 4) , (valor_max * 0.75))) 
 # modificar parametros con una lista
 st.write('Values:', values)
 
-
+#Constante para determinar el maximo de reviews
+reviews_max = int(df["number_of_reviews"].max())
+#Slider para seleccionar un minimo de reviews
+reviews = st.slider('Seleccione un minimo de reviews', 0, reviews_max, 0)
 
 # Mapa
 st.map(
-     df[((df["neighbourhood"] == option) 
-     & (df["room_type"] == option1[0])
+     df[((df["neighbourhood"] == barrio) 
+     & (df["room_type"] == hospedaje[0])
      & (df["price"] >= values[0]) 
-     & (df["price"] <= values[1]))])
+     & (df["price"] <= values[1])
+     & (df["number_of_reviews"] >= reviews))])
